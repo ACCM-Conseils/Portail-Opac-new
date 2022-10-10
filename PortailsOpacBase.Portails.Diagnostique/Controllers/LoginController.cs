c627b2afe96709ea69d617a7c608bdc96328984c
Clément Maldonado <contact@accm-conseils.info>
2022-10-10 23:32:32 +0200

    SAML

---
+++ PortailsOpacBase.Portails.Diagnostique/Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortailsOpacBase.Portails.Diagnostique.Controllers
+{
+    [Authorize]
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+
+            //You get the user’s first and last name below:
+            ViewBag.Name = userClaims?.FindFirst("name")?.Value;
+
+            // The 'preferred_username' claim can be used for showing the username
+            ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
+
+            // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
+            ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+            // TenantId is the unique Tenant Id - which represents an organization in Azure AD
+            ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+
+            String Email = String.Empty;
+            List<String> Profil = new List<String>();
+
+            foreach (System.Security.Claims.Claim claim in userClaims.Claims)
+            {
+                if (claim.Type.EndsWith("emailaddress"))
+                    Email = claim.Value;
+                else if (claim.Type.EndsWith("role"))
+                    Profil.Add(claim.Value);
+            }
+
+            return View();
+        }
+
+        public ActionResult Auth()
+        {
+            
+            return View();
+        }
+
+        // GET: Login
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        /// <summary>
+        /// Send an OpenID Connect sign-out request.
+        /// </summary>
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                    CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
--- PortailsOpacBase.Portails.Diagnostique/ModelOpac.Designer.cs
+++ PortailsOpacBase.Portails.Diagnostique/ModelOpac.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\conta\source\repos\ACCM-Conseils\PortailsOpacBase\PortailsOpacBase.Portails.Diagnostique\ModelOpac.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\maldo\source\repos\Altexence\PortailsOpacBase\PortailsOpacBase.Portails.Diagnostique\ModelOpac.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
